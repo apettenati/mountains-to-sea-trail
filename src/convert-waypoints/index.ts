5e0ed6e70545ad3cacd42834387dfe158cefd0d1
Amanda Pettenati <amanda@pette.net>
2021-04-16 01:17:42 -0400

    add papaparse csv file & remove node dependencies

--- src/convert-waypoints/index.ts
+++ src/convert-waypoints/index.ts
@@ -1,4 +1,4 @@
 
 export { totalDistance, getClosestTrackpoint, associateWaypointsWithTrackpoints, checkOutliers } from './convert-waypoints'
-export { Point, getTrackpointData, getWaypointData } from './import-data'
+// export { Point, getTrackpointData, getWaypointData } from './import-data'
 export { calcDistance, Miles } from './distance'
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import { 
   totalDistance, associateWaypointsWithTrackpoints, 
-  checkOutliers, getTrackpointData, getWaypointData, calcDistance 
+  checkOutliers, calcDistance, 
+  // getTrackpointData, getWaypointData
 } from './convert-waypoints'
 
 async function main() {
-  const trackpointFile = './segment-8-data/trackpoints-segment8.csv'
-  const waypointFile = './segment-8-data/waypoints-segment8.csv'
-  const trackpoints = await getTrackpointData(trackpointFile)
-  const waypoints = await getWaypointData(waypointFile)
-  const dist = calcDistance(trackpoints[0].coordinate, trackpoints[6574].coordinate)
-  const totalMiles = totalDistance(trackpoints)
-  // console.log({ dist, totalMiles })
-  const newWaypoints = associateWaypointsWithTrackpoints(waypoints, trackpoints)
-  // console.log({ newWaypoints })
-  const outliers = checkOutliers(newWaypoints, 0.05)
-  // console.log({ outliers })
+  // const trackpointFile = './segment-8-data/trackpoints-segment8.csv'
+  // const waypointFile = './segment-8-data/waypoints-segment8.csv'
+  // const trackpoints = await getTrackpointData(trackpointFile)
+  // const waypoints = await getWaypointData(waypointFile)
+  // const dist = calcDistance(trackpoints[0].coordinate, trackpoints[6574].coordinate)
+  // const totalMiles = totalDistance(trackpoints)
+  // // console.log({ dist, totalMiles })
+  // const newWaypoints = associateWaypointsWithTrackpoints(waypoints, trackpoints)
+  // // console.log({ newWaypoints })
+  // const outliers = checkOutliers(newWaypoints, 0.05)
+  // // console.log({ outliers })
+  console.log('test')
 }
 
 main()
